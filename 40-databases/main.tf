c187bb69d1241150849dfd54bef3da954ed2763d
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-10 21:11:30 +0530

    anisible-configuration

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -39,7 +39,7 @@ resource "terraform_data" "bootstrap" {
   }
   provisioner "remote-exec" {
     inline = [ 
-         "sudo sh /tmp/bootstrap.sh"
+         "sudo sh /tmp/bootstrap.sh mongodb"
      ]
   }
 }
