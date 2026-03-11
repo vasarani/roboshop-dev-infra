85f7708ad36f64c573c1e3203e8c16ddd893199f
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-11 11:14:13 +0530

    anisible

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -14,7 +14,7 @@ resource "aws_instance" "mongodb" {
 
 }
 
-resource "terraform_data" "bootstrap" {
+resource "terraform_data" "mongodb" {
   triggers_replace = [
     aws_instance.mongodb.id
   ]
@@ -64,7 +64,7 @@ resource "aws_instance" "redis" {
 
 }
 
-resource "terraform_data" "bootstrap_redis" {
+resource "terraform_data" "redis" {
   triggers_replace = [
     aws_instance.redis.id
   ]
@@ -95,3 +95,51 @@ resource "terraform_data" "bootstrap_redis" {
 }
 
 
+
+
+
+resource "aws_instance" "mysql" {
+  ami = local.ami_id
+  instance_type = "t3.micro"
+  subnet_id = local.database_subnet_id
+  vpc_security_group_ids = [local.mysql_sg_id]
+ # iam_instance_profile = aws_iam_instance_profile.bastion.name
+
+  tags = merge(
+    local.common_tags,
+    {
+        Name = "${var.project}-${var.environment}-mysql"
+    }
+  )
+
+}
+
+resource "terraform_data" "mysql" {
+  triggers_replace = [
+    aws_instance.mysql.id
+  ]
+
+  connection {
+    type = "ssh"
+    user = "ec2-user"
+    password = "DevOps321"
+    host = aws_instance.mysql.private_ip
+  }
+
+  provisioner "file" {
+    source = "bootstrap.sh" # Local file path
+    destination = "/tmp/bootstrap.sh" # Destination path on the remote machine
+  }
+
+  provisioner "remote-exec" {
+    inline = [ 
+        "chmod +x /tmp/bootstrap.sh"
+
+     ]
+  }
+  provisioner "remote-exec" {
+    inline = [ 
+         "sudo sh /tmp/bootstrap.sh mysql"
+     ]
+  }
+}
