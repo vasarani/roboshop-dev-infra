683be3f48f701265a01437b5c15002d9ede01bfc
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-11 15:04:47 +0530

    actions

--- 40-databases/locals.tf
+++ 40-databases/locals.tf
@@ -11,6 +11,7 @@ locals {
   mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+  rabbitmq_sg_id = data.aws_ami.joindevops.rabbitmq_sg_id.value
   mysql_role_name = join( "-", [ for name in ["${var.project}", "${var.environment}", "mysql"] : title(name)])
 
   mysql_policy_name = join( "", [ for name in ["${var.project}", "${var.environment}", "mysql"] : title(name)])
