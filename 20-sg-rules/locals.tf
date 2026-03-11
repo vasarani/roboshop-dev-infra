dd36b2b52e3dea5d07f6d891b6d75d6fe9211ccf
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-11 15:09:09 +0530

    actions

--- 20-sg-rules/locals.tf
+++ 20-sg-rules/locals.tf
@@ -6,5 +6,7 @@ locals {
   user_sg_id      = data.aws_ssm_parameter.user_sg_id.value
   redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
   mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
+  rabbitmq_sg_id = data.aws_ssm_parameter.rabbitmq_sg_id.value
+
 
 }
--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -58,3 +58,13 @@ resource "aws_security_group_rule" "mysql_bastion" {
   source_security_group_id = local.bastion_sg_id
   security_group_id        = local.mysql_sg_id
 }
+
+resource "aws_security_group_rule" "rabbitmq_bastion" {
+  type      = "ingress"
+  from_port = 22
+  to_port   = 22
+  protocol  = "tcp"
+  # where traffic is comming from
+  source_security_group_id = local.bastion_sg_id
+  security_group_id        = local.rabbitmq_sg_id
+}
