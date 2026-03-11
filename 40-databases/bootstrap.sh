7905ff7cd25e69c9da21d84f8ec6af050adf1f82
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-11 12:08:30 +0530

    actions

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -8,6 +8,7 @@ cd /home/ec2-user
 git clone https://github.com/vasarani/ansible-roboshop-roles-tf.git
 
 cd ansible-roboshop-roles-tf
+git pull
 
 ansible-playbook -e component=$component roboshop.yaml
 
