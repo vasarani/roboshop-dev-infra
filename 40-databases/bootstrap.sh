a86c842574fbdcf38f5d6900d5247168939c345c
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-11 11:51:21 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -3,6 +3,11 @@
 component=$1
 dnf install ansible -y 
 
+dnf install ansible -y
+dnf install python3-pip -y
+pip3 install boto3 botocore
+
+
 cd /home/ec2-user
 git clone https://github.com/vasarani/ansible-roboshop-roles-tf.git
 
