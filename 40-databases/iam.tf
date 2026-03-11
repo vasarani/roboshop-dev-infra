45ad4033ef54a09e8c2fb46b6212cb9f98315d5c
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-11 14:15:42 +0530

    actions

--- 40-databases/iam.tf
+++ 40-databases/iam.tf
@@ -26,7 +26,7 @@ resource "aws_iam_role" "mysql" {
 }
 
 resource "aws_iam_policy" "mysql" {
-  name = "S3ReadOnlyAccessPolicy"
+  name = local.mysql_policy_name
   description =  "A policy for MySQL Ec2 instance"
   policy = file("mysql-iam-policy.json")
 }
