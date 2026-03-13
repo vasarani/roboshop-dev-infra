454ebc04386e4f1dad3a17465f406dd8b46ae023
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-13 15:35:33 +0530

    terraform

---
+++ 60-catalogue/variables.tf
@@ -0,0 +1,17 @@
+variable "project" {
+  default = "Roboshop"
+}
+
+variable "environment" {
+  default = "Dev"
+}
+
+variable "zone_id" {
+  type = string
+  default = "Z07217272DEU08NPF6AZQ"
+}
+
+variable "domain_name" {
+  type = string
+  default = "rawsd.in"
+}
