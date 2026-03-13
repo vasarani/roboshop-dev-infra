e033abc7a8ac40006a483a861d44f1c4fbab9dec
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-13 15:48:04 +0530

    terraform

--- 60-catalogue/locals.tf
+++ 60-catalogue/locals.tf
@@ -1,7 +1,7 @@
 locals {
   catalogue_sg_id = data.aws_ssm_parameter.catalogue_sg_id.value
   private_subnet_id = split(",", data.aws_ssm_parameter.private_subnet_ids.value)[0]
-  ami_id = data.aws_ami.joindevops
+  ami_id = data.aws_ami.joindevops.id
   common_tags = {
     project     = "roboshop"
     environment = "dev"
