1253501600dd7ca06ef63ec003a041f1300c0b76
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-14 11:09:35 +0530

    terraform

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -158,8 +158,8 @@ resource "aws_autoscaling_group" "catalogue" {
     }
     )
     content {
-    key = each.key
-    value = each.value
+    key = tag.key
+    value = tag.value
     propagate_at_launch = true
     }
   }
